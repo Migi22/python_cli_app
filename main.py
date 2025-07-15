ec7e97116e5231605d958f0c809fad71c4ab5e47
Lance <miguel2002622@gmail.com>
2025-07-15 15:53:32 +0800

    Refactor welcome message formatting in main menu

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from alarm_clock import run as run_alarm_clock
 
 def main():
     while True:
-        print("\nWelcome to the Python CLI App developed by Lance Miguel Damalerio" \
-        "1. To-Do List" \
-        "2. Unit Converter" \
-        "3. Mini Quiz Game" \
-        "4. Password Generator" \
-        "5. Alarm Clock" \
-        "6. Exit")
+        print("""
+        Welcome to the Python CLI App developed by Lance Miguel Damalerio!
+        1. To-Do List
+        2. Unit Converter
+        3. Mini Quiz Game
+        4. Password Generator
+        5. Alarm Clock
+        6. Exit
+        """)
 
         choice = input("Enter your choice (1-6): ")
 
