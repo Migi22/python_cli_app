09fce795d175a4560ebe508b8cca8d5b40ad1785
Lance <miguel2002622@gmail.com>
2025-07-16 14:45:52 +0800

    Refactor task management: move load_tasks and save_task functions to utils.py

--- todo_list/add.py
+++ todo_list/add.py
@@ -1,23 +1,5 @@
 from datetime import datetime
-import json
-import os
-
-# Global name of the file so other functions in To-Do can use the same file
-TODO_FILE = "todo.json"
-
-# Will load the task
-def load_tasks():
-    """Load tasks from a JSON file. If not exists, return an empty list."""
-    if os.path.exists(TODO_FILE):
-        with open(TODO_FILE, "r") as file:
-            return json.load(file)
-    return []
-
-# Save the task to a JSON File
-def save_task(tasks):
-    """Save task list to JSON file."""
-    with open(TODO_FILE, "w") as file:
-        json.dump(tasks, file, indent=1)
+from .utils import load_tasks, save_task
 
 # Function for adding task
 def add_task():
