e53be62f1b60b3270c27b2fd8cb3eff3bf6b1ec2
Lance <miguel2002622@gmail.com>
2025-07-18 15:22:20 +0800

    Refactor run function: implement currency converter menu and user input handling

--- currency_converter/__init__.py
+++ currency_converter/__init__.py
@@ -1,2 +1,28 @@
+
+
 def run():
-    print("Unit Converter module running...") # placeholder logic for the actual Unit Converter implementation
+    print("\nLaunching Currency converter...")
+
+    # Welcome message and menu for the currency converter.
+    print("""
+          \nWelcome to the currency converter feature of the toolkit.
+          
+          Currency Converter Menu:
+          1. Exchange
+          2. Back to Menu
+          """)
+    
+    # Looping for the choice of the user until it breaks the loop
+    while True:
+        choose = input("Please select 1 or 2 to proceed: ").strip() # Prompt user for menu choice
+        
+        if choose == "1":
+            print("You selected currency exchange") #TODO: Remove this after the function is done. This is a placeholder
+            #currency_exchange()
+        elif choose == "2":
+            print("Going back to the menu")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+    
